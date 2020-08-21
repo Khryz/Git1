1cb1a7bb84dccd3ea027eff8f4f27bd0fde72b66
khryz <christianvalentevargas110898@gmail.com>
2020-08-21 03:27:54 -0500

    placeholder="Introduce 1er númplaceholder="Introduce 1er número"placeholder="Introduce 1er númeroceroi primer commit

---
+++ src/JS.js
@@ -0,0 +1,8 @@
+function x(){
+    var txt1 = parseInt(document.getElementById('txt1').value);
+    var txt2 = parseInt(document.getElementById('txt2').value);
+    var result;
+    result = txt1+txt2;
+    console.log(result);
+}
+
